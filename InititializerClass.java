4449f751fdca8801fd17823193f42c21eb17da03
samuelowino <samuelowino43@gmail.com>
2016-05-01 10:03:52 +0300

    init
    First Commit

---
+++ InititializerClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alpha;
+
+/**
+ *
+ * @author user
+ */
+public class InititializerClass {
+    
+    public static void main(String[] args) {
+        
+        mainInterface main = new mainInterface(true);
+    }
+}
